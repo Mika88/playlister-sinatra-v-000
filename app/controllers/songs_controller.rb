18fd167ed4632bc43a9ebc15d0487f89abdb2c2e
Mika88 <mikaspivak@gmail.com>
2018-11-28 01:04:10 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -11,7 +11,6 @@ class SongsController < ApplicationController
   end
 
   post '/songs' do
-    binding.pry
     @song = Song.create(params[:song])
     @song.artist = Artist.create(params[:artist])
     @song.save
